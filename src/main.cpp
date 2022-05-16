b12a8126b1055ff09722d611c60266adacaaba46
Sergio Pugliesi <sergiopugli@gmail.com>
2022-05-16 17:35:36 -0300

    teste reversão

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,26 @@
 #include <Arduino.h>
 
-#define A1A 6
+
+/**
+ * @brief Pinos de controle dos motores, os pinos foram escolhidos por 
+ * terem capacidade de realizar PWM, para controle de velocidade.
+ * São nomeados de acordo com as entradas do módulo de ponte H utilizado
+ * 
+ */
+#define A1A 6 
 #define A1B 5
 #define B1A 11
 #define B1B 10
 
-#define CW 1
-#define CCW 0
+/**
+ * @brief Definição auxiliar para sentido de rotação
+ * 
+ */
+#define CW 1 // Horário
+#define CCW 0 // Anti-horário
 
 void setup()
 {
-  // put your setup code here, to run once:
   pinMode(A1A, OUTPUT);
   pinMode(A1B, OUTPUT);
   pinMode(B1A, OUTPUT);
@@ -26,10 +36,25 @@ void rotateDigital(bool dir, byte pA, byte pB, bool en)
   digitalWrite(pHigh, en);
 }
 
+void stop()
+{
+  digitalWrite(A1A, LOW);
+  digitalWrite(A1B, LOW);
+  digitalWrite(B1A, LOW);
+  digitalWrite(B1B, LOW);
+
+}
+
 void loop()
 {
   rotateDigital(CW, A1A, A1B, true);
+  rotateDigital(CW, B1A, B1B, true);
   delay(1000);
-  rotateDigital(CW, A1A, A1B, false);
+  stop();
+  delay(3000);
+  rotateDigital(CCW, A1A, A1B, true);
+  rotateDigital(CCW, B1A, B1B, true);
+  delay(1000);
+  stop();
   delay(3000);
 }
